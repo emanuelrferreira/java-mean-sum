5dbda9a75408da3f985670d336df22aa7105d22e
Emanuel Ferreira <decoding.dino@gmail.com>
2025-11-15 17:44:24 +0000

    feat: add MeanSum instantiable class

---
+++ src/MeanSum.java
@@ -0,0 +1,50 @@
+/*
+MeanSum.java
+Mini program to calculate the sum and mean of an integer array
+*/
+
+public class MeanSum {
+
+    // vars
+    private int[] numbers;
+    private int sum;
+    private double mean;
+
+    // constructor
+    public MeanSum() {
+        // default values
+        numbers = new int[]{2, 4, 6, 8};
+        sum = 0;
+        mean = 0.0;
+    }
+
+    // compute
+    public void computeMeanSum() {
+        sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i]; // accumulate values
+        }
+
+        // cast to double to avoid integer division
+        mean = (double) sum / numbers.length;
+    }
+
+    // getters
+    public int getSum() {
+        return sum;
+    }
+
+    public double getMean() {
+        return mean;
+    }
+
+    // utility method to print the array
+    public void printArray() {
+        System.out.print("Array values: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
